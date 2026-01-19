9c11fed1c058a5d7439365fdccb3211d6c474daf
Justomoriconi <justomoriconi@gamil.com>
2026-01-19 12:16:43 -0300

    Agregue User_Model

---
+++ backend/Model/User_model.go
@@ -0,0 +1,10 @@
+package User
+
+type User struct {
+	IDUser   uint64 `gorm:"primaryKey;autoIncrement"`
+	Name     string `gorm:"type:varchar(120);not null"`
+	LastName string `gorm:"type:varchar(120);not null"`
+	Email    string `gorm:"type:varchar(120);not null;unique"`
+	Password string `gorm:"type:varchar(255);not null"`
+	Role     string `gorm:"type:enum('USER','ADMIN');default:'USER';not null"`
+}
