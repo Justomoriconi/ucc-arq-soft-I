226fc4d2462329a3ca743717aec26c41d3bb2dc8
Justomoriconi <justomoriconi@gamil.com>
2026-01-21 20:45:31 -0300

    Modifico  mapping.go

--- backend/app/mapping.go
+++ backend/app/mapping.go
@@ -3,6 +3,7 @@ package app
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
+	Controller "ucc-arq-soft-I/controllers/user"
 )
 
 func MapUrls(r *gin.Engine) {
@@ -11,4 +12,6 @@ func MapUrls(r *gin.Engine) {
 	api.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"message": "pong"})
 	})
+
+	api.POST("/signin", Controller.CreateUser)
 }
